71395a424cfb4d6821dd32a6e67e39963a35aa1c
eugenechiaay <eugene.chiay@gmail.com>
2022-10-17 17:28:57 +0800

    Update ReportsController.php
    configure reports controller

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ReportsCollection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
 
 class ReportsController extends Controller
 {
-    public function __invoke()
+    public function index()
     {
-        return Inertia::render('Reports/Index');
+        return Inertia::render('Reports/Index', [
+            'filters' => Request::all('search', 'trashed'),
+            'reports' => new ReportsCollection(
+                Auth::user()->account->reports()
+                    ->orderBy('title')
+                    ->filter(Request::only('search', 'trashed'))
+                    ->paginate()
+                    ->appends(Request::all())
+            ),
+        ]);
     }
 }
