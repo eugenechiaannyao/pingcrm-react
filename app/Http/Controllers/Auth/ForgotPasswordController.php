33d11b1e0f0fe7ef67610cecd8ffca5d647cf4ee
eugenechiaay <eugene.chiay@gmail.com>
2022-10-17 17:27:03 +0800

    Update ForgotPasswordController.php
    configure forgot passwords controller to display link request form, and to inject flash message based on success or failure of reset password email link sent

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class ForgotPasswordController extends Controller
@@ -22,13 +29,35 @@ class ForgotPasswordController extends Controller
 
     use SendsPasswordResetEmails;
 
-    public function showLinkRequestForm()
+
+    public function showLinkRequestForm(): \Inertia\Response
     {
         return Inertia::render('Auth/Passwords/Email');
     }
 
-    public function sendResetLinkEmail(){
-        return Inertia::render('Auth/Passwords/Email');
+    /**
+     * @throws ValidationException
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        if ($response == Password::RESET_LINK_SENT) {
+            $this->sendResetLinkResponse($request, $response);
+            return redirect()->route('login')->with('success', 'We have sent a password reset link!');
+        } else {
+            $this->sendResetLinkFailedResponse($request, $response);
+            return redirect()->route('forgot')->with('error', 'We don\'t recognise this email:(');
+            // White Screen pops up after echo...
+        }
     }
 
     /**
