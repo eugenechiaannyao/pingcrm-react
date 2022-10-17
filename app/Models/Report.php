2ff083bd6fc49fcf3bfef06087d04e93f77614d7
eugenechiaay <eugene.chiay@gmail.com>
2022-10-17 17:28:31 +0800

    Create Report.php
    Configure report model

---
+++ app/Models/Report.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Report extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where('title', 'like', '%'.$search.'%');
+        })->when($filters['trashed'] ?? null, function ($query, $trashed) {
+            if ($trashed === 'with') {
+                $query->withTrashed();
+            } elseif ($trashed === 'only') {
+                $query->onlyTrashed();
+            }
+        });
+    }
+}
